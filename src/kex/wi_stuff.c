2bfdf218bce40d4a2773fff0ae624e9d70db7091
svkaiser <svkaiser@b1809fa0-8a4d-0410-8880-862f83baeb0f>
2014-03-08 05:26:22 +0000

    + Reverting back previous change
    git-svn-id: svn://svn.code.sf.net/p/doom64ex/code/trunk@1287 b1809fa0-8a4d-0410-8880-862f83baeb0f

--- src/kex/wi_stuff.c
+++ src/kex/wi_stuff.c
@@ -202,10 +202,9 @@ int WI_Ticker(void) {
         cluster = P_GetCluster(gamemap);
         nextcluster = P_GetCluster(nextmap);
 
-        if(!nextcluster                                                         ||
-            ((nextcluster && cluster != nextcluster && nextcluster->enteronly)  ||
-            (cluster && cluster != nextcluster && !cluster->enteronly))) {
-                return ga_victory;
+        if((nextcluster && cluster != nextcluster && nextcluster->enteronly) ||
+            (cluster && cluster != nextcluster && !cluster->enteronly)) {
+            return ga_victory;
         }
 
         return 1;
